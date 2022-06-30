a026e05e91ae5c51eddcc01589e959fd9c4f9068
hb <notmyemail@letsstopthis.com>
2022-06-30 15:26:13 -0600

    modified main activity

--- TorifiedWrapper/src/main/java/org/torproject/android/sample/MainActivity.java
+++ TorifiedWrapper/src/main/java/org/torproject/android/sample/MainActivity.java
@@ -74,30 +74,6 @@ public class MainActivity extends Activity {
         WebView webView = findViewById(R.id.webview);
         TextView statusTextView = findViewById(R.id.status);
 
-        GenericWebViewClient webViewClient = new GenericWebViewClient();
-        webViewClient.setRequestCounterListener(requestCount ->
-                runOnUiThread(() -> statusTextView.setText("Request Count: " + requestCount)));
-        webView.setWebViewClient(webViewClient);
-        webView.setBackgroundColor(Color.WHITE); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
-        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
-        webView.getSettings().setLoadsImagesAutomatically(true); // automatically loads images to enhance wrapper experience
-        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE); // forces the wrapper to load from internet resources instead of cache which ensure you are seeing the most up-to-date information
-        webView.getSettings().setSupportMultipleWindows(false); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
-        webView.getSettings().getMediaPlaybackRequiresUserGesture(); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
-        webView.getSettings().setJavaScriptEnabled(true); // enables javascript - normally, this is a security no-no - however, the web-page you are wrapping is protected in an application sandbox, an isolated process, and with a certificate. the website would have to be the culprit for you to be truly concerned.
-        CookieManager.getInstance().setAcceptCookie(true); // gives the website you are wrapping the ability to set cookies so you don't have to log-in every time you open the wrapper
-        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, false); // prevents third-party cookies - usually these cookies are not necessary for a website to function
-        webView.getSettings().setSupportZoom(true);
-        webView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; Android 9; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.0.0 Mobile Safari/537.36"); // sets a user-agent for your wrapper that always works but limits the amount of information websites can collect on you
-        webView.setOverScrollMode(View.OVER_SCROLL_NEVER); // prevents choppy rendering when scrolling
-        webView.getSettings().setAllowContentAccess(false); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
-        webView.getSettings().setAllowFileAccess(false); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
-        webView.getSettings().setDomStorageEnabled(false); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
-        webView.clearHistory(); // a good practice to clear history - clears history every time the application boots up so you preserve your current session when in use
-        webView.clearCache(true); // a good practice to clear all cache to prevent loading issues
-        webView.clearFormData(); // a good practice to reset form data
-        webView.setScrollbarFadingEnabled(false); // forces the scrollbar to stay visible
-        webView.canGoBack();
         webView.setOnKeyListener(new View.OnKeyListener() {
 
             //Allows navigation
@@ -119,7 +95,11 @@ public class MainActivity extends Activity {
 
                 String status = intent.getStringExtra(TorService.EXTRA_STATUS);
                 Toast.makeText(context, status, Toast.LENGTH_SHORT).show();
-                webView.loadUrl("https://shmishmorsh.github.io/StaticWeb/"); //link to website you want to wrap
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    WebView.setWebContentsDebuggingEnabled(true);
+                }
+
 
 
             }
@@ -136,7 +116,7 @@ public class MainActivity extends Activity {
 
                 while ((conn = torService.getTorControlConnection()) == null) {
                     try {
-                        Thread.sleep(500);
+                        Thread.sleep(300);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -144,6 +124,37 @@ public class MainActivity extends Activity {
 
                 if (conn != null) {
                     Toast.makeText(MainActivity.this, "Got TOR connection, please wait.", Toast.LENGTH_LONG).show();
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    GenericWebViewClient webViewClient = new GenericWebViewClient();
+                    webViewClient.setRequestCounterListener(requestCount ->
+                            runOnUiThread(() -> statusTextView.setText("Request Count: " + requestCount)));
+                    webView.setWebViewClient(webViewClient);
+                    webView.setBackgroundColor(Color.WHITE); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
+                    webView.setLayerType(View.LAYER_TYPE_HARDWARE, null); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
+                    webView.getSettings().setLoadsImagesAutomatically(true); // automatically loads images to enhance wrapper experience
+                    webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE); // forces the wrapper to load from internet resources instead of cache which ensure you are seeing the most up-to-date information
+                    webView.getSettings().setSupportMultipleWindows(false); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
+                    webView.getSettings().getMediaPlaybackRequiresUserGesture(); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
+                    webView.getSettings().setJavaScriptEnabled(true); // enables javascript - normally, this is a security no-no - however, the web-page you are wrapping is protected in an application sandbox, an isolated process, and with a certificate. the website would have to be the culprit for you to be truly concerned.
+                    CookieManager.getInstance().setAcceptCookie(true); // gives the website you are wrapping the ability to set cookies so you don't have to log-in every time you open the wrapper
+                    CookieManager.getInstance().setAcceptThirdPartyCookies(webView, false); // prevents third-party cookies - usually these cookies are not necessary for a website to function
+                    webView.getSettings().setSupportZoom(true);
+                    webView.getSettings().setUserAgentString("Mozilla/5.0 (Linux; Android 9; Unspecified Device) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.0.0 Mobile Safari/537.36"); // sets a user-agent for your wrapper that always works but limits the amount of information websites can collect on you
+                    webView.setOverScrollMode(View.OVER_SCROLL_NEVER); // prevents choppy rendering when scrolling
+                    webView.getSettings().setAllowContentAccess(true); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
+                    webView.getSettings().setAllowFileAccess(true); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
+                    webView.getSettings().setDomStorageEnabled(true); // take a look at the documentation on the android studio developer page: https://developer.android.com/docs
+                    webView.clearHistory(); // a good practice to clear history - clears history every time the application boots up so you preserve your current session when in use
+                    webView.clearCache(true); // a good practice to clear all cache to prevent loading issues
+                    webView.clearFormData(); // a good practice to reset form data
+                    webView.setScrollbarFadingEnabled(false); // forces the scrollbar to stay visible
+                    webView.canGoBack();
+                    webView.loadUrl("https://aidaccess.org"); //link to website you want to wrap
+
                 }
 
             }
